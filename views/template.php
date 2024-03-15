92dd2bb00f127a73d3ec03f2663a5ea24bfd2537
Antoine Fontaine <antoine.fontaine@uha.fr>
2024-03-15 13:48:09 +0100

    fix frontend

--- views/template.php
+++ views/template.php
@@ -7,7 +7,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
     <header>
